8ea0b11a9fbffc4999a9da90ecf77aa210cb69d1
devinoue <itisagoodcat@gmail.com>
2023-02-03 01:46:40 +0900

    add: Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        ADD Staging NEXT_PUBLIC_API_BASE_URL:{" "}
+        Test NEXT_PUBLIC_API_BASE_URL:
         {process.env.NEXT_PUBLIC_API_BASE_URL}
         <br />
         API_URL: {process.env.API_URL}
